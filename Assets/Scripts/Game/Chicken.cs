68c8c1922c23bbd975f6ec87401cc22582e4b715
jmansquid1 <jasperfeasby@gmail.com>
2025-04-10 12:47:41 -0400

    It move, peck and stop

--- Assets/Scripts/Game/Chicken.cs
+++ Assets/Scripts/Game/Chicken.cs
@@ -1,10 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum State : byte
+{
+    pecking,
+    running
+}
 
 public class Chicken : Animal
 {
+    private float peckTimer = 0;
+    public float minPeckBreak = 5;
+    public float maxPeckBreak = 10;
+    public float peckingTime = 2;
+
+
     [SerializeField] private Blackboard blackboard;
 
     Target target;
@@ -16,7 +28,20 @@ public class Chicken : Animal
 
     private void Update()
     {
+        if (peckTimer <= 0)//when the time is out the chicken pecks
+        {
+            blackboard.SetOrAddValue<bool>("CanPeck", true);
+            target.IsPaused = true;
+        }
+        if (peckTimer <= -peckingTime)//after one second the chicken stops pecking
+        {
+            blackboard.SetOrAddValue<bool>("CanPeck", false);
+            target.IsPaused = false;
+            peckTimer = UnityEngine.Random.Range(minPeckBreak, maxPeckBreak);
 
+        }
+        Debug.Log(peckTimer);
+        peckTimer -= Time.deltaTime;
     }
 
     // Update is called once per frame
@@ -30,6 +55,7 @@ public class Chicken : Animal
 
     protected override void OnStart()
     {
+        peckTimer = UnityEngine.Random.Range(minPeckBreak, maxPeckBreak);
         blackboard.SetOrAddValue<bool>("CanPeck", false);
         target = GetComponent<Target>();
     }
